4679a50ae9cabcc4fe532b7206b45ed8e4b97d91
Rok Mokotar <mokotar.rok@gmail.com>
2025-01-05 21:24:04 +0100

    Prepare search spaces

--- constants/search_space.py
+++ constants/search_space.py
@@ -31,19 +31,53 @@ GRID_SEARCH_SPACE_TEST = {
     "bilateral_grid": [True, False],
 }
 
-
-GRID_SEARCH_SPACE_TEST_0 = {
-    "max_steps": [500, 1000],
-    "num_points": [16 * 16, 32 * 32],
-    "init_type": ["random", "grid"],
-}
-
+# Lets first figure out if the strategy is working
+# After you are done, update the default values in config.py
 GRID_SEARCH_SPACE_TEST_1 = {
-    "number_iterations": [500, 1000, 2500, 5000, 10000],
-    "number_ipo": [1024],
+    # Optimization settings
+    "group_optimization": [True, False],
+    "strategy": [None, "default", "mcmc"],
 }
 
+# Lets find how are losses affecting the training
 GRID_SEARCH_SPACE_TEST_2 = {
-    "number_iterations": [1000],
-    "eps2d": [2, 4, 8, 16],
+    "loss_weights": [
+        [1 / 3, 1 / 3, 1 / 3],
+        [1 / 2, 1 / 4, 1 / 4],
+        [1 / 4, 1 / 2, 1 / 4],
+        [1 / 4, 1 / 4, 1 / 2],
+    ],
+    "normal_loss_weight": [None, 0.1, 0.5, 1.0],
+    "distortion_loss_weight": [None, 0.1, 0.5, 1.0],
+}
+
+# Lets figure out if the regularization helps
+GRID_SEARCH_SPACE_TEST_3 = {
+    "scale_regulation": [None, 0.1, 0.5, 1.0],
+    "opacity_regulation": [None, 0.1, 0.5, 1.0],
+}
+
+# What about initializations?
+GRID_SEARCH_SPACE_TEST_4 = {
+    "init_type": ["random", "grid", "knn"],
+    "num_points": [256, 512, 1024, 2048],
+    "extent": [1.0, 2.0, 4.0],
+    "init_scale": [0.5, 1.0, 2.0],
+    "init_opacity": [0.5, 1.0, 2.0],
+}
+
+# Main thingies (you can omit 2dgs-inria since it might have some issues)
+GRID_SEARCH_SPACE_TEST_5 = {
+    # Training settings
+    "max_steps": [250, 500, 1000, 2500, 5000, 10000],
+    "learning_rate": [None, 1e-4, 1e-3, 1e-2, 1e-1],
+    # Model configuration
+    "model_type": ["2dgs", "2dgs-inria", "3dgs"],
+}
+
+GRID_SEARCH_SPACE_TEST_4 = {
+    "sh_degree": [None, 3],
+    "selective_adam": [True, False],
+    "sparse_gradient": [True, False],
+    "bilateral_grid": [True, False],
 }

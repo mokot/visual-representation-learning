d55d84113dc7de9d3c34b4bc59d92bcca30ca7df
FedeHR <fedeharjes@gmail.com>
2025-01-14 21:24:28 +0100

    Updated config according to latest hyperparam tuning and prepared for final training.

--- configs/config.py
+++ configs/config.py
@@ -16,36 +16,36 @@ class Config:
     batch_size: int = 1
 
     # Results and logs
-    save_results: Optional[bool] = True
-    results_path: Optional[Path] = Path("./results")
-    save_logs: Optional[bool] = True
-    logs_path: Optional[Path] = Path("./logs")
+    save_results: Optional[bool] = False
+    results_path: Optional[Path] = Path("./results_grid_init_train")
+    save_logs: Optional[bool] = False
+    logs_path: Optional[Path] = Path("./logs_grid_init_train")
 
     # Training settings
     max_steps: int = 1_000
-    learning_rate: Optional[float] = 1e-3  # Only for group optimization
+    learning_rate: Optional[float] = 0.01  # Only for group optimization
     loss_weights: List[float] = field(
         default_factory=lambda: [1 / 2, 0, 1 / 2]
     )  # [L1, L2, SSIM]
-    normal_loss_weight: Optional[float] = None  # Not for 3DGS
-    distortion_loss_weight: Optional[float] = None  # Not for 3DGS
+    normal_loss_weight: Optional[float] = 0.14  # Not for 3DGS, new default value from latest hyperparameter tuning
+    distortion_loss_weight: Optional[float] = 0.13  # Not for 3DGS, new default value from latest hyperparameter tuning
 
     # Regularization settings
-    scale_regularization: Optional[float] = None
-    opacity_regularization: Optional[float] = None
+    scale_regularization: Optional[float] = 0.1  # New default value from latest hyperparameter tuning
+    opacity_regularization: Optional[float] = 0.5  # New default value from latest hyperparameter tuning
 
     # Gaussian initialization and parameters
     init_type: Literal["random", "grid", "knn"] = (
-        "random"  # `means` are not learnable if `grid`
+        "grid"  # `means` are not learnable if `grid`
     )
     num_points: int = 1_024
-    extent: float = 2.0
-    init_scale: float = 1.0
-    init_opacity: float = 1.0
+    extent: float = 2.0  # New default value from latest hyperparameter tuning
+    init_scale: float = 2.0  # New default value from latest hyperparameter tuning
+    init_opacity: float = 0.6  # New default value from latest hyperparameter tuning
 
     # Model configuration
     model_type: Literal["2dgs", "2dgs-inria", "3dgs"] = "2dgs"
-    sh_degree: Optional[int] = None
+    sh_degree: Optional[int] = 1  # New default value from latest hyperparameter tuning
 
     # Optimization settings
     group_optimization: bool = True  # If `True`, strategy is ignored
